d7043a3e13e3414bec642a246b1503173eb75017
Prasad Muley <muleyprasad00@gmail.com>
2026-08-15 19:03:50 +0530

    app version console added

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import packageJson from '../package.json';
+
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
+
+console.log('App Version:', packageJson.version);
